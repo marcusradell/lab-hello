d0bce4d82c2e718157a067617718f98a933a31dc
Marcus Rådell <marcus@radell.net>
2024-04-02 17:21:02 +0200

    hero card added

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,8 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
+import { Sunshiney } from "next/font/google";
 import "./globals.css";
 
-const inter = Inter({ subsets: ["latin"] });
+const font = Sunshiney({ subsets: ["latin"], weight: "400" });
 
 export const metadata: Metadata = {
   title: "Sol Rådell",
@@ -16,7 +16,7 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={font.className}>{children}</body>
     </html>
   );
 }
